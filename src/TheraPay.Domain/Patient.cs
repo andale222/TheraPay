b94de1c9bcc275cf8382352d5da92a324419a9b6
hildebrandt <andre.hildebrandt@tuhh.de>
2026-02-16 09:34:01 +0100

    add Patient class and test list creation

---
+++ src/TheraPay.Domain/Patient.cs
@@ -0,0 +1,6 @@
+﻿namespace TheraPay.Domain;
+
+public class Patient
+{
+    
+}
--- tests/TheraPay.Core.Tests/PatientOperations_test.cs
+++ tests/TheraPay.Core.Tests/PatientOperations_test.cs
@@ -1,12 +1,21 @@
 using System.Reflection;
+using TheraPay.Domain;
 
 namespace TheraPay.Core.Tests;
+
 
 public class PatientOperations_test
 {
     [Fact]
     public void EmptyPatientRepository_AddPatient_Count1()
     {
-        Assert.True(true);
+        // GIVEN
+        var patients = new List<Patient>();
+
+        // WHEN
+        patients.Add(new Patient());
+
+        // THEN
+        Assert.Equal(1, patients.Count);
     }
 }
