f1bd268f38cb55e76ad66dd60616e34bdf4ea280
hildebrandt <andre.hildebrandt@tuhh.de>
2026-03-01 15:03:18 +0100

    add duration setting and testing

--- src/TheraPay.Domain/Appointment.cs
+++ src/TheraPay.Domain/Appointment.cs
@@ -2,14 +2,29 @@ namespace TheraPay.Domain;
 
 public class Appointment
 {
+    private const int MaxDurationInMinutes = 24*60; // 1 Day
     public Guid Id { get; }
     public DateTime Date { get; private set; }
     public string PatientID { get; private set; }
+    public int DurationInMinutes { get; private set; }
 
     public Appointment(DateTime date, string patientID)
     {
         Date = date;
         PatientID = patientID;
         Id = Guid.NewGuid();
+    }
+
+    public void SetDuration(int durationInMinutes)
+    {
+        if (durationInMinutes < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(durationInMinutes), "Duration cannot be negative.");
+        }
+        if (durationInMinutes > MaxDurationInMinutes)
+        {
+            throw new ArgumentOutOfRangeException(nameof(durationInMinutes), $"Duration cannot exceed {MaxDurationInMinutes} minutes.");
+        }
+        DurationInMinutes = durationInMinutes;
     }
 }
--- tests/TheraPay.Domain.Tests/Appointment_test.cs
+++ tests/TheraPay.Domain.Tests/Appointment_test.cs
@@ -4,6 +4,7 @@ namespace TheraPay.Domain.Tests;
 
 public class Appointment_test
 {
+    public static Appointment CreateAppointment() => new Appointment(new DateTime(2026, 1, 1, 14, 0, 0), "patientID");
     [Fact]
     public void GivenAppointmentData_CreateAppointment_AppointmentHasCorrectValues()
     {
@@ -19,4 +20,49 @@ public class Appointment_test
         Assert.Equal(patient.ID, appointment.PatientID);
         Assert.NotEqual(Guid.Empty, appointment.Id);
     }
+    [Fact]
+    public void GivenAppointment_AddDuration_DurationHasCorrectValue()
+    {
+        // GIVEN
+        var appointment = CreateAppointment();
+
+        // WHEN
+        appointment.SetDuration(50);
+
+        // THEN
+        Assert.Equal(50, appointment.DurationInMinutes);
+    }
+    [Fact]
+    public void GivenAppointment_OverrideDuration_DurationHasCorrectValue()
+    {
+        // GIVEN
+        var appointment = CreateAppointment();
+
+        // WHEN
+        appointment.SetDuration(50);
+        appointment.SetDuration(10);
+
+        // THEN
+        Assert.Equal(10, appointment.DurationInMinutes);
+    }
+    [Fact]
+    public void GivenAppointment_AddNegativeDuration_ThrowsException()
+    {
+        // GIVEN
+        var appointment = CreateAppointment();
+
+        // WHEN THEN
+        Assert.Throws<ArgumentOutOfRangeException>(() => appointment.SetDuration(-10));
+
+    }
+    [Fact]
+    public void GivenAppointment_AddTooLongDuration_ThrowsException()
+    {
+        // GIVEN
+        var appointment = CreateAppointment();
+
+        // WHEN THEN
+        Assert.Throws<ArgumentOutOfRangeException>(() => appointment.SetDuration(25 * 60)); // 25 hours is too long
+    }
+
 }
