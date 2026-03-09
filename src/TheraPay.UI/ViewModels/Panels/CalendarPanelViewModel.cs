2fc02b81fa937f7178ef155bb9f14c0448b4c701
hildebrandt <andre.hildebrandt@tuhh.de>
2026-03-09 09:52:04 +0100

    show patientId of appointment

--- src/TheraPay.UI/ViewModels/Panels/CalendarPanelViewModel.cs
+++ src/TheraPay.UI/ViewModels/Panels/CalendarPanelViewModel.cs
@@ -93,7 +93,7 @@ public sealed class CalendarPanelViewModel : ViewModelBase
                 Id = appt.Id.ToString(),
                 Date = appt.Date.ToString("dd.MM.yy HH:mm"),
                 Duration = $"{appt.DurationInMinutes} min",
-                PatientName = "TODO:",
+                PatientName = $"{appt.PatientID}",
                 AppointmentName = "TODO:"
             });
         }
