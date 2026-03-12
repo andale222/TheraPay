a409bb8ffe3720c73cc544104df5e42169bd7f69
hildebrandt <andre.hildebrandt@tuhh.de>
2026-03-12 22:41:37 +0100

    add practice data store and container

---
+++ src/TheraPay.Domain/PracticeData.cs
@@ -0,0 +1,14 @@
+namespace TheraPay.Domain;
+
+public class PracticeData
+{
+    public string Name { get; set; } = "practice name placeholde";
+    public string FirstNamePractitioner { get; set; } = "first name practitioner placeholder";
+    public string LastNamePractitioner { get; set; } = "last name practitioner placeholder";
+    public string StreetAndNumber { get; set; } = "street and number placeholder";
+    public string CityAndPostalCode { get; set; } = "city and postal code placeholder";
+    public string Country { get; set; } = "country placeholder";
+    public string PhoneNumber { get; set; } = "phone number placeholder";
+    public string IBAN { get; set; } = "IBAN placeholder";
+    public string TaxIdentificationNumber { get; set; } = "tax identification number placeholder";
+}
---
+++ src/TheraPay.Infrastructure/csv/records/CsvPracticeDataRecord.cs
@@ -0,0 +1,13 @@
+
+public sealed class CsvPracticeDataRecord
+{
+    public string Name { get; set; } = string.Empty;
+    public string FirstNamePractitioner { get; set; } = string.Empty;
+    public string LastNamePractitioner { get; set; } = string.Empty;
+    public string StreetAndNumber { get; set; } = string.Empty;
+    public string CityAndPostalCode { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string IBAN { get; set; } = string.Empty;
+    public string TaxIdentificationNumber { get; set; } = string.Empty;
+}
