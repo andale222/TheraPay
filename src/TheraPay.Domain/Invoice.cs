9af7250e4eebf33e03cc4d201870777b752b50b6
hildebrandt <andre.hildebrandt@tuhh.de>
2026-03-16 21:39:46 +0100

    add invoice status and tests

--- src/TheraPay.Domain/Invoice.cs
+++ src/TheraPay.Domain/Invoice.cs
@@ -5,14 +5,29 @@ public class Invoice
     public InvoicePatientData PatientData { get; private set; }
     public InvoiceAppointmentData AppointmentData { get; private set; }
     public Guid Id { get; }
+    public InvoiceStatus Status { get; private set; }
 
     public Invoice(InvoicePatientData patientData, InvoiceAppointmentData appointmentData)
     {
         Id = Guid.NewGuid();
         PatientData = patientData;
         AppointmentData = appointmentData;
+        Status = InvoiceStatus.Draft;
+    }
+
+    private bool IsEditable( ) => Status == InvoiceStatus.Draft;
+
+    public void Issue()
+    {
+        if (!IsEditable())
+            return;
+
+        Status = InvoiceStatus.Issued;
     }
 }
+
+
+public enum InvoiceStatus { Draft, Issued, Cancelled };
 
 public sealed record InvoicePatientData
 {
--- tests/TheraPay.Domain.Tests/Invoice_test.cs
+++ tests/TheraPay.Domain.Tests/Invoice_test.cs
@@ -7,13 +7,28 @@ public class Invoice_test
     public void GivenInvoicesPatientData_CreateInvoice_InvoiceHasCorrectValues()
     {
         // GIVEN
-        DateTime date = new DateTime(2026, 1, 1, 14, 0, 0);
         Patient patient = new Patient("A", "J", "L5R");
-        Appointment appointment = new Appointment( date, patient.ID);
         var patientData = new InvoicePatientData()
         {
             PatientName = patient.FirstName+" "+patient.LastName
         };
+        var appointmentData = new InvoiceAppointmentData();
+
+    //     // WHEN
+        var invoice = new Invoice(patientData, appointmentData);
+
+    //     // THEN
+        Assert.NotEqual(Guid.Empty, invoice.Id);
+        Assert.Equal(patientData.PatientName, invoice.PatientData.PatientName);
+    }
+    [Fact]
+    public void GivenInvoicesAppointmentData_CreateInvoice_InvoiceHasCorrectAppointmentData()
+    {
+        // GIVEN
+        DateTime date = new DateTime(2026, 1, 1, 14, 0, 0);
+        string patientID = "OR5";
+        Appointment appointment = new Appointment( date, patientID);
+        var patientData = new InvoicePatientData();
         var appointmentData = new InvoiceAppointmentData()
         {
             PatientId = appointment.PatientID,
@@ -26,8 +41,34 @@ public class Invoice_test
     //     // THEN
         Assert.NotEqual(Guid.Empty, invoice.Id);
         Assert.Equal(patientData.PatientName, invoice.PatientData.PatientName);
-        Assert.Equal(appointment.PatientID, invoice.AppointmentData.PatientId);
-        Assert.Equal(appointment.Date, invoice.AppointmentData.Date);
+        Assert.Equal(patientID, invoice.AppointmentData.PatientId);
+        Assert.Equal(date, invoice.AppointmentData.Date);
     }
-    
+    [Fact]
+    public void GivenEmptyData_CreateInvoice_InvoiceIsDraft()
+    {
+        // GIVEN
+        var patientData = new InvoicePatientData();
+        var appointmentData = new InvoiceAppointmentData();
+
+        // WHEN
+        var invoice = new Invoice(patientData, appointmentData);
+
+        // THEN
+        Assert.Equal(InvoiceStatus.Draft, invoice.Status);
+    }
+    [Fact]
+    public void GivenEmptyInvoice_IssueInvoice_StatusIsIssued()
+    {
+        // GIVEN
+        var patientData = new InvoicePatientData();
+        var appointmentData = new InvoiceAppointmentData();
+        var invoice = new Invoice(patientData, appointmentData);
+
+        // WHEN
+        invoice.Issue();
+
+        // THEN
+        Assert.Equal(InvoiceStatus.Issued, invoice.Status);
+    }
 }
