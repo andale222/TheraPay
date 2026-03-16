3adda08d8f31a97910fc6511bfbba2c126726781
hildebrandt <andre.hildebrandt@tuhh.de>
2026-03-16 21:06:07 +0100

    add single appointment data

--- src/TheraPay.Domain/Invoice.cs
+++ src/TheraPay.Domain/Invoice.cs
@@ -3,16 +3,23 @@ namespace TheraPay.Domain;
 public class Invoice
 {
     public InvoicePatientData PatientData { get; private set; }
+    public InvoiceAppointmentData AppointmentData { get; private set; }
     public Guid Id { get; }
 
-    public Invoice( InvoicePatientData patientData )
+    public Invoice(InvoicePatientData patientData, InvoiceAppointmentData appointmentData)
     {
         Id = Guid.NewGuid();
         PatientData = patientData;
+        AppointmentData = appointmentData;
     }
 }
 
 public sealed record InvoicePatientData
 {
     public string PatientName { get; set; } = "";
+}
+public sealed record InvoiceAppointmentData
+{
+    public DateTime Date { get; set;}
+    public string PatientId { get; set; } = "";
 }
--- tests/TheraPay.Domain.Tests/Invoice_test.cs
+++ tests/TheraPay.Domain.Tests/Invoice_test.cs
@@ -7,20 +7,27 @@ public class Invoice_test
     public void GivenInvoicesPatientData_CreateInvoice_InvoiceHasCorrectValues()
     {
         // GIVEN
-    //     DateTime date = new DateTime(2026, 1, 1, 14, 0, 0);
+        DateTime date = new DateTime(2026, 1, 1, 14, 0, 0);
         Patient patient = new Patient("A", "J", "L5R");
+        Appointment appointment = new Appointment( date, patient.ID);
         var patientData = new InvoicePatientData()
         {
             PatientName = patient.FirstName+" "+patient.LastName
         };
+        var appointmentData = new InvoiceAppointmentData()
+        {
+            PatientId = appointment.PatientID,
+            Date = appointment.Date
+        };
 
     //     // WHEN
-        var invoice = new Invoice(patientData);
+        var invoice = new Invoice(patientData, appointmentData);
 
     //     // THEN
-    //     Assert.Equal(patient.ID, appointment.PatientID);
         Assert.NotEqual(Guid.Empty, invoice.Id);
         Assert.Equal(patientData.PatientName, invoice.PatientData.PatientName);
+        Assert.Equal(appointment.PatientID, invoice.AppointmentData.PatientId);
+        Assert.Equal(appointment.Date, invoice.AppointmentData.Date);
     }
     
 }
