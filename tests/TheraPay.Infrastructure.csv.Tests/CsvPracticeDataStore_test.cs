b405737d5657812868929ded799df02ec83b7b9e
hildebrandt <andre.hildebrandt@tuhh.de>
2026-03-20 14:41:24 +0100

    add address splitup to practice data

--- tests/TheraPay.Infrastructure.csv.Tests/CsvPracticeDataStore_test.cs
+++ tests/TheraPay.Infrastructure.csv.Tests/CsvPracticeDataStore_test.cs
@@ -30,11 +30,17 @@ public class CsvPracticeDataStore_test
         Assert.Equal("Privatpraxis test", practiceData.Name);
         Assert.Equal("Mara", practiceData.FirstNamePractitioner);
         Assert.Equal("Mustermann", practiceData.LastNamePractitioner);
-        Assert.Equal("Teststraße 81", practiceData.StreetAndNumber);
-        Assert.Equal("12345 Teststadt", practiceData.CityAndPostalCode);
+        Assert.Equal("Teststraße", practiceData.Street);
+        Assert.Equal("81", practiceData.HouseNumber);
+        Assert.Equal("12345", practiceData.PostalCode);
+        Assert.Equal("Teststadt", practiceData.City);
         Assert.Equal("Deutschland", practiceData.Country);
+        Assert.Equal("2. OG", practiceData.AddressAdditional);
         Assert.Equal("+49 12345678901", practiceData.PhoneNumber);
         Assert.Equal("DE12 3456 7890 1234 5678 9012", practiceData.IBAN);
+        Assert.Equal("11112222", practiceData.BLZ);
+        Assert.Equal("Testbank", practiceData.BankName);
+        Assert.Equal("Therapie Januar", practiceData.Subject);
         Assert.Equal("123456789", practiceData.TaxIdentificationNumber);
         Assert.Equal(21, practiceData.DefaultPaymentTermDays);
     }
@@ -67,11 +73,17 @@ public class CsvPracticeDataStore_test
             Name = "Test Practice",
             FirstNamePractitioner = "John",
             LastNamePractitioner = "Doe",
-            StreetAndNumber = "123 Test Street",
-            CityAndPostalCode = "45678 Test City",
+            Street = "Test Street",
+            HouseNumber = "123",
+            PostalCode = "45678",
+            City = "Test City",
             Country = "Test Country",
+            AddressAdditional = "Floor 3",
             PhoneNumber = "+1 234 567 8901",
             IBAN = "DE12 3456 7890 1234 5678 9012",
+            BLZ = "99887766",
+            BankName = "Example Bank",
+            Subject = "Invoice Payment",
             TaxIdentificationNumber = "123456789",
             DefaultPaymentTermDays = 30
         };
@@ -84,11 +96,17 @@ public class CsvPracticeDataStore_test
         Assert.Equal(practiceData.Name, loadedPracticeData.Name);
         Assert.Equal(practiceData.FirstNamePractitioner, loadedPracticeData.FirstNamePractitioner);
         Assert.Equal(practiceData.LastNamePractitioner, loadedPracticeData.LastNamePractitioner);
-        Assert.Equal(practiceData.StreetAndNumber, loadedPracticeData.StreetAndNumber);
-        Assert.Equal(practiceData.CityAndPostalCode, loadedPracticeData.CityAndPostalCode);
+        Assert.Equal(practiceData.Street, loadedPracticeData.Street);
+        Assert.Equal(practiceData.HouseNumber, loadedPracticeData.HouseNumber);
+        Assert.Equal(practiceData.PostalCode, loadedPracticeData.PostalCode);
+        Assert.Equal(practiceData.City, loadedPracticeData.City);
         Assert.Equal(practiceData.Country, loadedPracticeData.Country);
+        Assert.Equal(practiceData.AddressAdditional, loadedPracticeData.AddressAdditional);
         Assert.Equal(practiceData.PhoneNumber, loadedPracticeData.PhoneNumber);
         Assert.Equal(practiceData.IBAN, loadedPracticeData.IBAN);
+        Assert.Equal(practiceData.BLZ, loadedPracticeData.BLZ);
+        Assert.Equal(practiceData.BankName, loadedPracticeData.BankName);
+        Assert.Equal(practiceData.Subject, loadedPracticeData.Subject);
         Assert.Equal(practiceData.TaxIdentificationNumber, loadedPracticeData.TaxIdentificationNumber);
         Assert.Equal(practiceData.DefaultPaymentTermDays, loadedPracticeData.DefaultPaymentTermDays);
 
