5a17101740b1dfa5b9ad2f1bf74be2e7538a1ec9
hildebrandt <andre.hildebrandt@tuhh.de>
2026-03-22 21:53:15 +0100

    refactor testData setups

--- tests/TheraPay.Core.Tests/TestData.cs
+++ tests/TheraPay.Core.Tests/TestData.cs
@@ -5,44 +5,13 @@ using TheraPay.Domain;
 
 public static class TestData
 {
-
-    public static Patient Patient1( ) => new Patient("A", "J", "L5R");
-    public static Patient Patient2( ) => new Patient("second", "patient", "NR2");
-
-
-    public static InMemoryPatientRepository getInMemoryPatientRepositoryWithTwoPatients()
-    {
-        InMemoryPatientRepository repository = new InMemoryPatientRepository();
-        repository.Add(Patient1());
-        repository.Add(Patient2());
-        return repository;
-    }
-    public static PatientService getPatientServiceWithInMemoryPatientRepositoryWithTwoPatients()
-    {
-        
-        InMemoryPatientRepository repository = getInMemoryPatientRepositoryWithTwoPatients();
-        return new PatientService(repository);
-    }
-
-
-
-    public static Appointment Appointment1( ) => new Appointment(new DateTime(2026, 1, 1, 14, 0, 0), Patient1().ID);
-    public static Appointment Appointment2( ) => new Appointment(new DateTime(2026, 1, 8, 15, 0, 0), Patient2().ID);
-
-    public static AppointmentService getAppointmentServiceWithInMemoryAppointmentRepositoryWithTwoAppointments()
-    {
-        InMemoryAppointmentRepository repository = new InMemoryAppointmentRepository();
-        Appointment appointment1 = Appointment1();
-        appointment1.SetDuration(60);
-        Appointment appointment2 = Appointment2();
-        appointment2.SetDuration(30);
-        repository.Add(appointment1);
-        repository.Add(appointment2);
-        return new AppointmentService(repository);
-    }
-
-
+    // Domain setups
+    public static Patient Patient1() => new Patient("A", "J", "L5R");
+    public static Patient Patient2() => new Patient("second", "patient", "NR2");
+    public static Appointment Appointment1() => new Appointment(new DateTime(2026, 1, 1, 14, 0, 0), Patient1().ID);
+    public static Appointment Appointment2() => new Appointment(new DateTime(2026, 1, 8, 15, 0, 0), Patient2().ID);
     public static Appointment Appointment1_2( ) => new Appointment(new DateTime(2026, 2, 1, 14, 0, 0), Patient1().ID);
+
     public static PracticeData PracticeData1() => new PracticeData()
         {
             Name = "Physio Praxis 7",
@@ -78,5 +47,37 @@ public static class TestData
 
         return invoice;
     }
+
+
+    // Core Set-Ups 
+    public static InMemoryPatientRepository getInMemoryPatientRepositoryWithTwoPatients()
+    {
+        InMemoryPatientRepository repository = new InMemoryPatientRepository();
+        repository.Add(Patient1());
+        repository.Add(Patient2());
+        return repository;
+    }
+    public static PatientService getPatientServiceWithInMemoryPatientRepositoryWithTwoPatients()
+    {
+
+        InMemoryPatientRepository repository = getInMemoryPatientRepositoryWithTwoPatients();
+        return new PatientService(repository);
+    }
+
+
+
+
+    public static AppointmentService getAppointmentServiceWithInMemoryAppointmentRepositoryWithTwoAppointments()
+    {
+        InMemoryAppointmentRepository repository = new InMemoryAppointmentRepository();
+        Appointment appointment1 = Appointment1();
+        appointment1.SetDuration(60);
+        Appointment appointment2 = Appointment2();
+        appointment2.SetDuration(30);
+        repository.Add(appointment1);
+        repository.Add(appointment2);
+        return new AppointmentService(repository);
+    }
+
 
 }
