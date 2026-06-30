e2bc0c4a52c5c7341375f8be815489ab88d690a1
hildebrandt <andre.hildebrandt@tuhh.de>
2026-06-30 21:06:01 +0200

    add billing number variant for comfort

--- src/TheraPay.Domain/BillingNumber.cs
+++ src/TheraPay.Domain/BillingNumber.cs
@@ -59,6 +59,7 @@ public static class BillingNumberCatalog
         new("0", 1.00m, 110.00m, "Psychotherapeutisches Erstgespräch", BillingNumberType.Privat),
         new("801a", 2.30m, 14.57m, "Erhebung des aktuellen psychischen Befundes", BillingNumberType.Privat),
         new("812a", 2.30m, 29.14m, "Psychotherapeutische Sprechstunde (25 Min.)", BillingNumberType.Privat),
+        new("812a", 2.30m, 29.14m, "Psychotherapeutische Kurzzeittherapie (25 Min.)", BillingNumberType.Privat),
         new("860", 2.30m, 53.62m, "Biographische Anamnese", BillingNumberType.Privat),
         new("861", 2.30m, 40.22m, "Tiefenpsychologisch fundierte Psychotherapie, Einzelbehandlung (50 Min.)", BillingNumberType.Privat),
         new("870", 2.30m, 43.72m, "Verhaltenstherapie, Einzelbehandlung (50 Min.)", BillingNumberType.Privat),
